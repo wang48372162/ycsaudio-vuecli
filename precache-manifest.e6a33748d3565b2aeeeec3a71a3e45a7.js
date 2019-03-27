9fd669b08598ddaa362ff1456417651b3e914a79
wang48372162 <wang48372162@gmail.com>
2019-03-27 12:51:45 +0800

    Updates

---
+++ precache-manifest.e6a33748d3565b2aeeeec3a71a3e45a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/ycsaudio/robots.txt"
+  },
+  {
+    "revision": "da1ac452ff0745d9afdf",
+    "url": "/ycsaudio/js/chunk-vendors.2758f890.js"
+  },
+  {
+    "revision": "b2a18cb1793591332de5",
+    "url": "/ycsaudio/js/app.28d347cc.js"
+  },
+  {
+    "revision": "ea182028f9ed5baad0399a19970f93bc",
+    "url": "/ycsaudio/index.html"
+  },
+  {
+    "revision": "44aae1c4429ea2282a831544908e5b25",
+    "url": "/ycsaudio/img/logo.44aae1c4.png"
+  },
+  {
+    "revision": "da1ac452ff0745d9afdf",
+    "url": "/ycsaudio/css/chunk-vendors.41f665b7.css"
+  },
+  {
+    "revision": "b2a18cb1793591332de5",
+    "url": "/ycsaudio/css/app.70be3be0.css"
+  }
+];
